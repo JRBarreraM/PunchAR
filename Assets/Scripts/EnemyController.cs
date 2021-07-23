a14e77b0ce7200df3ffba8651597b24bebe5d93b
Juan Oropeza <jaoc1811@gmail.com>
2021-07-22 22:14:25 -0400

    Started AI implementation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,25 +12,51 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        StartCoroutine(DoAttack());
     }   
-
 
     public void DoLeftHook(){
         StartCoroutine(DoAnimation("LeftHook"));
     }
+
     public void DoRightHook(){
         StartCoroutine(DoAnimation("RightHook"));
     }
+
     public void DoRightBlock(){
         StartCoroutine(DoAnimation("RightBlock"));
     }
+
     public void DoLeftBlock(){
         StartCoroutine(DoAnimation("LeftBlock"));
     }
+
     IEnumerator DoAnimation(string action){
         animator.SetBool(action, true);
         yield return new WaitForSeconds(0.5f);
         animator.SetBool(action, false);
+    }
+
+    IEnumerator DoAttack() {
+        while(true) {
+            yield return new WaitForSeconds(2f);
+            Attack();
+        }
+    }
+
+    void Attack() {
+        float number = Random.Range(0,11);
+        if (number >= 6) {
+            int attack = Random.Range(1,3);
+            switch (attack){
+                case 1:
+                    DoLeftHook();
+                    break;
+                case 2:
+                    DoRightHook();
+                    break;
+            }
+        }
     }
 
     // Update is called once per frame
